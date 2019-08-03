082e8f0aa68a095e78e1363c2942e544d3ef16e7
Mark Berney <berneydwmark@gmail.com>
2019-08-03 16:21:26 +1200

    Added Note.tsx + NoteList.tsx

---
+++ personal-management-app/src/components/NoteList.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Note from '../components/Note';
+
+class NoteList extends Component {
+    render() {
+        return(
+            <div className="noteList">
+
+            </div>
+        )
+    }
+}
+
+export default NoteList;

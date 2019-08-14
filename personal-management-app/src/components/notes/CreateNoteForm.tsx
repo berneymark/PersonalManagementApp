87bea3310046425497d5cf4a9f840236b5e99b4d
Mark Berney <berneydwmark@gmail.com>
2019-08-14 15:49:43 +1200

    Basic form added

--- personal-management-app/src/components/notes/CreateNoteForm.tsx
+++ personal-management-app/src/components/notes/CreateNoteForm.tsx
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
 
 interface States {
-    noteName: string;
+    noteTitle: string;
     noteContent: string;
 }
 
 class CreateNoteForm extends Component<{}, States> {
+    handleSubmit = () => {
+        
+    }
+
     render() {
         return(
             <div className="createNoteContainer">
-                <form></form>
+                <form onSubmit={ this.handleSubmit }>
+                    <input 
+                        type="text"
+                        name="noteTitle"
+                        placeholder="note title"></input>
+                    <button
+                        type="submit"
+                        name="noteSubmit">
+                        Create Note
+                    </button>
+                </form>
             </div>
         );
     }

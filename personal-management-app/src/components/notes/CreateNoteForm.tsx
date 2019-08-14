d6f5d94fb0182148ee76dbe74b9f3f1314ea1ef8
Mark Berney <berneydwmark@gmail.com>
2019-08-14 15:30:14 +1200

    Initial CreateNoteForm.tsx

---
+++ personal-management-app/src/components/notes/CreateNoteForm.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CreateNoteForm extends Component {
+    render() {
+        return(
+            <div className="createNoteContainer">
+                <form></form>
+            </div>
+        );
+    }
+}
+
+export default CreateNoteForm;
